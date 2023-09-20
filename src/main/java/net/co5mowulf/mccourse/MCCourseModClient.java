faa764088cfc98bbde9d45de3540730d600d8e85
Co5mowulf <Co5mowulf@gmail.com>
2023-09-20 16:03:30 -0400

    Update

--- src/main/java/net/co5mowulf/mccourse/MCCourseModClient.java
+++ src/main/java/net/co5mowulf/mccourse/MCCourseModClient.java
@@ -1,6 +1,9 @@
 package net.co5mowulf.mccourse;
 
 import net.co5mowulf.mccourse.MCCourseMod;
+import net.co5mowulf.mccourse.block.entity.ModBlockEntities;
+import net.co5mowulf.mccourse.block.entity.renderer.GemEmpoweringBlockEntityRenderer;
+import net.co5mowulf.mccourse.networking.ModMessages;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
@@ -16,6 +19,7 @@ import net.co5mowulf.mccourse.screen.ModScreenHandlers;
 import net.co5mowulf.mccourse.util.ModModelPredicateProvider;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 import net.minecraft.client.util.ModelIdentifier;
 
 public class MCCourseModClient implements ClientModInitializer {
@@ -42,5 +46,9 @@ public class MCCourseModClient implements ClientModInitializer {
         ModelLoadingRegistry.INSTANCE.registerModelProvider((manager, out) -> new ModelIdentifier(MCCourseMod.MOD_ID, "radiation_staff_3d", "inventory"));
 
         HandledScreens.register(ModScreenHandlers.GEM_EMPOWERING_SCREEN_HANDLER, GemEmpoweringScreen::new);
+
+        ModMessages.registerS2CPackets();
+
+        BlockEntityRendererFactories.register(ModBlockEntities.GEM_EMPOWERING_STATION_BE, GemEmpoweringBlockEntityRenderer::new);
     }
 }
