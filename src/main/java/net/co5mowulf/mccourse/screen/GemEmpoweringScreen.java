8db72f1297a7bed48f3d0d8718a75a601a33a7c8
Co5mowulf <Co5mowulf@gmail.com>
2023-09-20 15:50:48 -0400

    Update

--- src/main/java/net/co5mowulf/mccourse/screen/GemEmpoweringScreen.java
+++ src/main/java/net/co5mowulf/mccourse/screen/GemEmpoweringScreen.java
@@ -3,10 +3,13 @@ package net.co5mowulf.mccourse.screen;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.co5mowulf.mccourse.MCCourseMod;
 import net.co5mowulf.mccourse.screen.renderer.EnergyInfoArea;
+import net.co5mowulf.mccourse.screen.renderer.FluidStackRenderer;
 import net.co5mowulf.mccourse.util.MouseUtil;
 import net.fabricmc.fabric.api.client.screen.v1.Screens;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
@@ -19,6 +22,7 @@ public class GemEmpoweringScreen extends HandledScreen<GemEmpoweringScreenHandle
             new Identifier(MCCourseMod.MOD_ID, "textures/gui/gem_empowering_station_gui.png");
 
     private EnergyInfoArea energyInfoArea;
+    private FluidStackRenderer fluidStackRenderer;
 
     public GemEmpoweringScreen(GemEmpoweringScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -30,6 +34,11 @@ public class GemEmpoweringScreen extends HandledScreen<GemEmpoweringScreenHandle
         titleY = 1000;
         playerInventoryTitleY = 1000;
         assignEnergyInfoArea();
+        assignFluidStackRenderer();
+    }
+
+    private void assignFluidStackRenderer() {
+        fluidStackRenderer = new FluidStackRenderer((FluidConstants.BUCKET / 81) * 64, true, 16, 39);
     }
 
     private void assignEnergyInfoArea() {
@@ -44,12 +53,20 @@ public class GemEmpoweringScreen extends HandledScreen<GemEmpoweringScreenHandle
         }
     }
 
+    private void renderFluidTooltip(DrawContext context, int mouseX, int mouseY, int x, int y, int offsetX, int offsetY, FluidStackRenderer renderer) {
+        if(isMouseAboveArea(mouseX, mouseY, x, y, offsetX, offsetY, renderer)) {
+            context.drawTooltip(Screens.getTextRenderer(this), renderer.getTooltip(handler.blockEntity.fluidStorage, TooltipContext.Default.BASIC),
+                    Optional.empty(), mouseX - x, mouseY - y);
+        }
+    }
+
     @Override
     protected void drawForeground(DrawContext context, int mouseX, int mouseY) {
         int x = (width - backgroundWidth) / 2;
         int y = (height - backgroundHeight) / 2;
 
         renderEnergyAreaTooltips(context, mouseX, mouseY, x, y);
+        renderFluidTooltip(context, mouseX, mouseY, x, y, 26, 11, fluidStackRenderer);
     }
 
     @Override
@@ -65,6 +82,8 @@ public class GemEmpoweringScreen extends HandledScreen<GemEmpoweringScreenHandle
         renderProgressArrow(context, x, y);
 
         energyInfoArea.draw(context);
+        fluidStackRenderer.drawFluid(context, handler.blockEntity.fluidStorage, x + 25, y + 11, 16, 39,
+                (FluidConstants.BUCKET / 81) * 64);
     }
 
     private void renderProgressArrow(DrawContext context, int x, int y) {
@@ -78,6 +97,10 @@ public class GemEmpoweringScreen extends HandledScreen<GemEmpoweringScreenHandle
         renderBackground(context);
         super.render(context, mouseX, mouseY, delta);
         drawMouseoverTooltip(context, mouseX, mouseY);
+    }
+
+    private boolean isMouseAboveArea(int pMouseX, int pMouseY, int x, int y, int offsetX, int offsetY, FluidStackRenderer renderer) {
+        return MouseUtil.isMouseOver(pMouseX, pMouseY, x + offsetX, y + offsetY, renderer.getWidth(), renderer.getHeight());
     }
 
     private boolean isMouseAboveArea(int pMouseX, int pMouseY, int x, int y, int offsetX, int offsetY, int width, int height) {
